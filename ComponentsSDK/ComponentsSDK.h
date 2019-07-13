e0c13900da5073cf235abe23c845192caf20849b
Frank <frank@MacBook-Pro.local>
2019-07-13 17:54:47 +0800

    add ComponentsSDK

---
+++ ComponentsSDK/ComponentsSDK.h
@@ -0,0 +1,19 @@
+//
+//  ComponentsSDK.h
+//  ComponentsSDK
+//
+//  Created by Frank on 2019/7/13.
+//  Copyright © 2019 Frank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ComponentsSDK.
+FOUNDATION_EXPORT double ComponentsSDKVersionNumber;
+
+//! Project version string for ComponentsSDK.
+FOUNDATION_EXPORT const unsigned char ComponentsSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ComponentsSDK/PublicHeader.h>
+
+
